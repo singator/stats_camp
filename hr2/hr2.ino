00c4ab079c3b9c7b21cc5cc6b2ad3d4de853e401
Vik <stavg@nus.edu.sg>
2017-04-01 19:03:12 +0800

    modified:   hr2/hr2.ino 	modified:   plotting_serial/plotting_serial.pde 	new file:   test_serial_read3.py - writes to csv
    +with open('some.csv', 'w', newline='') as f:
    +    writer = csv.writer(f)
    +    writer.writerows(y2[1:])
    +
    \ No newline at end of file

--- hr2/hr2.ino
+++ hr2/hr2.ino
@@ -1,35 +1,31 @@
 // a simple program to print the heart rate at every pulse.
 
 const byte interruptPin = 2;
-// volatile byte state = LOW;
+volatile byte state = LOW;
 unsigned long time1, time2;
 byte count=0;
 volatile unsigned long hr = 0.0;
 
 void setup() {
-  // pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
   Serial.begin(9600);
   //Serial.println("Get ready...");
   delay(5000);
   time1 = millis();
-  // Serial.print("Start time: ");
-  // Serial.println(time1); 
 }
 
 void loop() {
-  // digitalWrite(LED_BUILTIN, state);
+  digitalWrite(LED_BUILTIN, state);
 }
 
 void blink() {
-  if (count > 0) {
     //Serial.print("HR: ");
     time2 = millis();
     hr = 60000 / (time2 - time1);
-    Serial.println(hr);
+    Serial.print(hr);
+    Serial.print(",");
+    Serial.println(time2);
     time1 = time2;
-  } else {
-    count += 1;
-  }
 }
---
+++ test_serial_read3.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 16:48:21 2017
+
+@author: stavg
+"""
+
+import serial
+import csv
+
+ser = serial.Serial('COM6', 9600, timeout=2)
+y2=[]
+
+while True:
+    try:
+        y = ser.readline()
+        print(y)
+        y2.append(y.decode().strip()) # remove the trailing characters
+        
+    except KeyboardInterrupt:
+        break
+
+ser.close()
+
+y2 = [x.split(',') for x in y2]
+
+with open('some.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(y2[1:])
+    
