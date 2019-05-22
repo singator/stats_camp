c81b93275419f6fab4bdaf8e5979c3e650578b87
macbook-air-vik <viknesh.gopal@gmail.com>
2019-05-22 06:49:00 +0200

    modified:   data_upload/app.R     - uses isolate() correctly.     - plot displays upon clicking upload button     - add text display, conditional on features 	modified:   src/hrv2.Rmd

--- data_upload/app.R
+++ data_upload/app.R
@@ -8,6 +8,19 @@
 #
 
 library(shiny)
+library(RHRV)
+library(tidyverse)
+
+# Creates rhrv object, without removing outliers, before interpolation
+create_nihr <- function(fname) {
+  obj <- CreateHRVData()
+  vec1 <- read.table(fname)[,1]
+  vec1 <- vec1 - vec1[1]
+  obj <- LoadBeatVector(obj, vec1, scale = 0.001)
+  obj <- BuildNIHR(obj)
+  obj <- SetVerbose(obj, TRUE)
+  obj
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,15 +36,17 @@ ui <- fluidPage(
                          value=1.0, min=0, max=2,step=0.01)),
      column(width=6,
             numericInput("weight", "Weight (kg)", value=50, min=0, max=150,
-                         step=0.1)),
+                         step=0.1))
+   ),
+   fluidRow(
      column(width=4,
             radioButtons("gender", "Gender", choices=c("Female", "Male"),
                          inline = TRUE))
    ),
    hr(),
    fluidRow(
-     column(width=4, textInput("person_id", "Person ID", value="")),
-     column(width=4, textInput("dev_id", "Device ID", value=""))
+     column(width=6, textInput("person_id", "Name", value="")),
+     column(width=6, textInput("dev_id", "Device ID", value=""))
    ),
    hr(),
    fluidRow(
@@ -39,24 +54,40 @@ ui <- fluidPage(
      column(width=6, selectInput("treatment", "Treatment",
                                  choices=c("baseline", "after_stairs",
                                            "watching_sports", 
-                                           "listening_music", "math")))
+                                           "listening_music", "doing_math")))
    ),
    hr(),
    fluidRow(
      column(width=6, actionButton("upload", "Upload"))
+   ),
+   hr(),
+   fluidRow(
+     column(width=6, plotOutput('plot1')),
+     column(width=6, textOutput('text1'))
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  rhrv_obj <- reactive({
+    in_file <- input$file1
+    if(is.null(in_file))
+      return(NULL)
+    rhrv_obj <- create_nihr(in_file$datapath)
+    rhrv_obj
+  })
+  
+   output$plot1 <- renderPlot({
+     input$upload
+     isolate(
+     if(is.null(rhrv_obj())) {
+       #dist <- rnorm(100)
+       #hist(dist)
+       return(NULL)
+     } else {
+       PlotNIHR(rhrv_obj())
+     })
    })
 }
 
